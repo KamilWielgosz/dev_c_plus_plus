8caf90ff0809e9f28543f2ab1ee15277b81721f9
bigos1995-95 <bigos1995-95@tlen.pl>
2016-07-12 18:20:53 +0200

    Add files via upload

---
+++ Testowanie GetsyncKeyState/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+using namespace std; 
+
+int main(){ 
+  int   x = 30; 
+  int   y; 
+        while( true) { 
+   if ( x > 0) { 
+         
+        cout <<"super x jest wieksze od 0"<<endl; 
+        x--; 
+        y++; 
+   } 
+    if (y == 5) {
+    cout<<"super y jest wieksze od 0"<<endl; 
+    } 
+} 
+}
